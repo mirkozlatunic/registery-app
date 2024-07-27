99aef0fa1eafe3cb0250f73767d72b593098b99c
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-07-27 16:14:53 -0700

    get the data from the Loader to the HomePage

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,3 +1,10 @@
+import type { HomeLaoderResult } from "./homeLoader";
+import { useLoaderData } from "react-router-dom";
+
 export default function HomePage() {
+  const { featuredPackages } = useLoaderData() as HomeLaoderResult;
+
+  console.log(featuredPackages);
+
   return <div>Home Page</div>;
 }
